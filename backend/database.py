e8077a4c1bf77106a06104cba16b985dd878391f
nepsefloorsheet-ops <nepsefloorsheet@gmail.com>
2026-02-09 13:26:22 +0545

    Add files via upload

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,5 @@
 import os
+from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
@@ -7,7 +8,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=True, 
+    future=True,
+    connect_args={"statement_cache_size": 0},
+    poolclass=NullPool
+)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
